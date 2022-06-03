d7cf8f88f14b5665fdcc0ccde75e43e19b4bad25
Brian4755 <brian.mccune1@yahoo.com>
2022-06-02 19:14:00 -0500

    add handleClick function

--- js/app.js
+++ js/app.js
@@ -1,5 +1,14 @@
 /*-------------------------------- Constants --------------------------------*/
-let winningCombos
+let winningCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
 
 /*---------------------------- Variables (state) ----------------------------*/
 let board
@@ -20,7 +29,10 @@ let messageEl = document.getElementById('message')
 /*-------------------------------- Functions --------------------------------*/
 init()
 function init() {
-  board = [null, null, null, null, null, null, null, null, null]
+  board = [
+    null, null, null,
+    null, null, null,
+    null, null, null]
   turn = 1
   winner = null
   render()
@@ -29,7 +41,6 @@ function init() {
 function render() {
 
   board.forEach(function(square, i) {
-    console.log(squareEls[i])
     if (square === 1) {
       squareEls[i].textContent = 'X'
     } 
@@ -37,16 +48,36 @@ function render() {
       squareEls[i].textContent = 'O'
     }
   })
+  renderMessage()
 }
 
 function renderMessage() {
   if (winner === null) {
     messageEl.textContent = `It is player ${turn === 1 ? 'X' : 'O'}'s turn`
-  }
-  if (winner === 'T') {
+  } else if (winner === 'T') {
     messageEl.textContent = "It's a tie"
-  }
-  else {
+  } else {
     messageEl.textContent = `Player ${turn === 1 ? 'O' : 'X'} won!`
   }
+}
+
+
+squareEls.forEach(function(square) {
+  square.addEventListener('click', handleClick)
+  handClick()
+  let sqIdx = target.evt
+  if (board[sqIdx] !== null) {
+    return
+  }
+  else if (winner !== null) {
+    return
+  } else {
+    return (turn*-1)
+  }
+  let winner = getWinner
+  render()
+})
+
+function handleClick(evt) {
+
 }
